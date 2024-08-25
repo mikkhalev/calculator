5db4319fc072e8d2f96f24ee6b6ce8fbc98eef21
mikkhalev <a.mikkhalev@list.ru>
2024-08-25 21:55:53 +0300

    Initial commit: complete pet project

---
+++ src/components/buttons-panel/button-panel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from "./buttons-panel.module.scss";
+const ButtonsPanel = () => {
+    return (
+        <div className={classes.panel}>
+
+        </div>
+    );
+};
+
+export default ButtonsPanel;
---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calculator = () => {
+    return (
+        <div cl>
+
+        </div>
+    );
+};
+
+export default Calculator;
